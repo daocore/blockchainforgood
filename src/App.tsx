d1488483de4461bbf082b35402682982a2047100
right <daocoredev@gmail.com>
2024-04-17 13:58:55 +0800

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import { Home } from './home'
+import { Analytics } from "@vercel/analytics/react"
 
 function App() {
-  return ( <div className='min-h-screen flex justify-center relative'>
+
+  return (<div className='min-h-screen flex justify-center relative'>
     <div className='h-10 w-full absolute bg-main' style={{ filter: "blur(80px)" }}></div>
+    {window?.location?.href?.includes("https://blockchainforgood.xyz/") && <Analytics />}
     <Home />
-  </div> )
+  </div>)
 }
 
 export default App
