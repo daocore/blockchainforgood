cd7731650aa3ec3a1909af27fb55c1dceb00e00a
right <daocoredev@gmail.com>
2024-05-26 12:21:03 +0800

    update

--- src/pages/form/index.tsx
+++ src/pages/form/index.tsx
@@ -1,8 +1,14 @@
 import { Loading } from "../../components/Loading";
 
-const isProduction = window?.location?.href?.includes("blockchainforgood");
-export const organizationalChannel = isProduction ? `https://www.moledao.io/#/form/project/create/230ac677-083f-4240-8f61-6a3e0d504ffd` : `http://localhost:3000/#/form/project/create/090ba7c2-bc0f-4476-b58e-fe834118dd89}`
-export const personalChannel = isProduction ? `https://www.moledao.io/#/form/individual/create/230ac677-083f-4240-8f61-6a3e0d504ffd` : `http://localhost:3000/#/form/individual/create/090ba7c2-bc0f-4476-b58e-fe834118dd89`
+const isProduction = window?.location?.href?.includes("blockchainforgood.xyz");
+const isBeta = window?.location?.href?.includes("blockchainforgood.verce");
+const devUrl = isBeta ? "https://beta.moledao.io" : "http://localhost:3000";
+
+const devId = "090ba7c2-bc0f-4476-b58e-fe834118dd89"; // beta
+// const devId = "972ac263-0dd9-43cc-97d0-ea7fbe7477f5"; // localhost
+
+export const organizationalChannel = isProduction ? `https://www.moledao.io/#/form/project/create/230ac677-083f-4240-8f61-6a3e0d504ffd` : `${devUrl}/#/form/project/create/${devId}`
+export const personalChannel = isProduction ? `https://www.moledao.io/#/form/individual/create/230ac677-083f-4240-8f61-6a3e0d504ffd` : `${devUrl}/#/form/individual/create/${devId}`
 
 export const OrgForm = () => {
     return (
