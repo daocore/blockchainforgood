9edcd8bc5897ade1f05ab07ccc2b18846a35ae3d
melon <melon@192.168.1.183>
2024-06-18 16:14:02 +0800

    feat: share card

--- app/news/detail/[id]/back.tsx
+++ app/news/detail/[id]/back.tsx
@@ -1,26 +1,22 @@
 "use client";
 import { useRouter } from "next/navigation";
 import { ChevronLeft } from "lucide-react";
-import { useIsMobile } from "@/hooks";
 
 export function GoBack() {
   const router = useRouter();
   const onBackToIndex = () => {
     router.back();
   };
-  const isMobile = useIsMobile();
 
   return (
-    isMobile && (
-      <div className="mb-8 mt-4">
-        <span
-          className="text-xs inline-flex cursor-pointer"
-          onClick={onBackToIndex}
-        >
-          <ChevronLeft size={16} />
-          Back
-        </span>
-      </div>
-    )
+    <div className="mb-8 mt-4 block md:hidden">
+      <span
+        className="text-xs inline-flex cursor-pointer"
+        onClick={onBackToIndex}
+      >
+        <ChevronLeft size={16} />
+        Back
+      </span>
+    </div>
   );
 }
