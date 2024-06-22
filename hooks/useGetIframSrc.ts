0e1a79f1cb74d01522554c2128091ce751d20b60
right <daocoredev@gmail.com>
2024-06-22 18:57:02 +0800

    fix:env error

--- hooks/useGetIframSrc.ts
+++ hooks/useGetIframSrc.ts
@@ -1,6 +1,9 @@
-import { BASE_URL, LOCATION_HOST } from "@/constants";
+import { LOCATION_HOST } from "@/constants";
 import { useSearchParams } from "next/navigation";
 import { useEffect, useState } from "react";
+
+const PRODUCTION_MOLEDAO_DOMAIN = "//www.blockchainforgood.xyz"
+const PRODUCTION_VERCEL_DOMAIN = "//blockchainforgood.xyz"
 
 interface IOptions {
   hashPaths: string,
@@ -17,8 +20,9 @@ export function useGetInfamSrc({ hashPaths, eventIds }: IOptions) {
 
   useEffect(() => {
     const hasOneTimeCode = searchParams.has("code");
+    const isProdENV = [PRODUCTION_MOLEDAO_DOMAIN, PRODUCTION_VERCEL_DOMAIN]?.includes(window?.location?.host);
 
-    const latestHref = `${LOCATION_HOST}/${hashPaths}/${process.env.NODE_ENV === "production" ? eventIds.production : eventIds?.dev}`;
+    const latestHref = `${LOCATION_HOST}/${hashPaths}/${isProdENV ? eventIds.production : eventIds?.dev}`;
     const oneTimeCode = searchParams.get("code");
 
     setChannel(
