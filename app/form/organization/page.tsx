253584b39b7454b2efc0762ce6ab08b78f4b4dd0
right <daocoredev@gmail.com>
2024-06-27 19:49:23 +0800

    fix

--- app/form/organization/page.tsx
+++ app/form/organization/page.tsx
@@ -3,7 +3,7 @@ import { IFrameLoading, Loading } from "@/components/Loading";
 import { useGetInfamSrc } from "@/hooks";
 import { Suspense } from "react";
 
-const ORGANIZATION_HASH_PATH = "#/form/individual";
+const ORGANIZATION_HASH_PATH = "#/form/project";
 const PRODUCTION_EVENT_ID = "230ac677-083f-4240-8f61-6a3e0d504ffd";
 const DEV_EVENT_ID = "090ba7c2-bc0f-4476-b58e-fe834118dd89"; //beta
 // const DEV_EVENT_ID = "972ac263-0dd9-43cc-97d0-ea7fbe7477f5"; //zflocal
