57033d95981a48e8fca46ddb36e114f89e478722
melon <melon@MelonMacBook-Pro.local>
2024-07-01 16:04:36 +0800

    feat: add sitemap & robots

--- constants/url.ts
+++ constants/url.ts
@@ -3,3 +3,5 @@ export const LOCATION_HOST = process.env.NEXT_PUBLIC_LOCATION_HOST
 
 export const API_URL = BASE_URL + "/api";
 export const IMAGE_URL = BASE_URL + "/assets";
+
+export const VISITED_PRODUCTION_URL = "https://www.blockchainforgood.xyz/"
