d5e2b1c72cb55908370341b956c3e81984f0dfc9
melon <melon@MelonMacBook-Pro.local>
2024-08-02 15:05:00 +0800

    feat: add news type

--- app/news/constants.ts
+++ app/news/constants.ts
@@ -5,6 +5,7 @@ export const NEWS_TYPE_NAME = {
   [NEWS_TYPE.RREAL_TALK_CASE_STUDY]: 'Real Talk Case Study',
   [NEWS_TYPE.BGA_PROGRESS_PULSE]: 'BGA Progress Pulse',
   [NEWS_TYPE.BFG_BGA]: 'BFG x BGA',
+  [NEWS_TYPE.KLIMADAO_BGA]: 'Klimadao X BGA',
 };
 
 export const NEWS_PLATFORM_NAME = {
