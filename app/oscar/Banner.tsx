f7f3b2ce174ed22bf6559bc46e96a676e7952453
melon <melon@MelonMacBook-Pro.local>
2024-08-28 18:44:39 +0800

    oscar

---
+++ app/oscar/Banner.tsx
@@ -0,0 +1,265 @@
+"use client";
+
+import OscarImage1 from "@/assets/oscar/oscar1.png";
+import OscarImage2 from "@/assets/oscar/oscar2.png";
+import OscarImage3 from "@/assets/oscar/oscar3.png";
+import OscarImage4 from "@/assets/oscar/oscar4.png";
+import OscarImage5 from "@/assets/oscar/oscar5.png";
+import OscarImage6 from "@/assets/oscar/oscar6.png";
+import OscarImage7 from "@/assets/oscar/oscar7.png";
+import OscarImage8 from "@/assets/oscar/oscar8.png";
+import OscarImage9 from "@/assets/oscar/oscar9.png";
+import OscarImage10 from "@/assets/oscar/oscar10.png";
+import OscarImage11 from "@/assets/oscar/oscar11.png";
+import OscarImage12 from "@/assets/oscar/oscar12.png";
+import OscarImage13 from "@/assets/oscar/oscar13.png";
+import OscarImage14 from "@/assets/oscar/oscar14.png";
+import OscarImage15 from "@/assets/oscar/oscar15.png";
+import OscarImage16 from "@/assets/oscar/oscar16.png";
+import OscarImage17 from "@/assets/oscar/oscar17.png";
+
+import OscarIcon1 from "@/assets/oscar/oscar-icon1.png";
+import OscarIcon2 from "@/assets/oscar/oscar-icon2.png";
+import OscarIcon3 from "@/assets/oscar/oscar-icon3.png";
+import OscarIcon4 from "@/assets/oscar/oscar-icon4.png";
+import OscarIcon5 from "@/assets/oscar/oscar-icon5.png";
+import OscarIcon6 from "@/assets/oscar/oscar-icon6.png";
+import OscarIcon7 from "@/assets/oscar/oscar-icon7.png";
+import OscarIcon8 from "@/assets/oscar/oscar-icon8.png";
+import OscarIcon9 from "@/assets/oscar/oscar-icon9.png";
+import OscarIcon10 from "@/assets/oscar/oscar-icon10.png";
+import OscarIcon11 from "@/assets/oscar/oscar-icon11.png";
+import OscarIcon12 from "@/assets/oscar/oscar-icon12.png";
+import OscarIcon13 from "@/assets/oscar/oscar-icon13.png";
+import OscarIcon14 from "@/assets/oscar/oscar-icon14.png";
+import OscarIcon15 from "@/assets/oscar/oscar-icon15.png";
+import OscarIcon16 from "@/assets/oscar/oscar-icon16.png";
+import OscarIcon17 from "@/assets/oscar/oscar-icon17.png";
+import { cn } from "@/lib";
+import styles from "./styles.module.css";
+import Image from "next/image";
+import { useEffect, useRef, useState } from "react";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+} from "@/components/ui/carousel";
+import Autoplay from "embla-carousel-autoplay";
+import Fade from "embla-carousel-fade";
+import { type CarouselApi } from "@/components/ui/carousel";
+
+const bannerList = [
+  {
+    cover: OscarImage1,
+    icon: OscarIcon1,
+    title: "NO POVERTY",
+  },
+  {
+    cover: OscarImage2,
+    icon: OscarIcon2,
+    title: "Zero Hunger",
+  },
+  {
+    cover: OscarImage3,
+    icon: OscarIcon3,
+    title: "Good Health And Well-Being",
+  },
+  {
+    cover: OscarImage4,
+    icon: OscarIcon4,
+    title: "Quality Education",
+  },
+  {
+    cover: OscarImage5,
+    icon: OscarIcon5,
+    title: "Gender Equality",
+  },
+  {
+    cover: OscarImage6,
+    icon: OscarIcon6,
+    title: "Clean Water And Sanitation",
+  },
+  {
+    cover: OscarImage7,
+    icon: OscarIcon7,
+    title: "Affordable And Clean Energy",
+  },
+  {
+    cover: OscarImage8,
+    icon: OscarIcon8,
+    title: "Decent Work And Economic Growth",
+  },
+  {
+    cover: OscarImage9,
+    icon: OscarIcon9,
+    title: "Industry And Infrastructure",
+  },
+  {
+    cover: OscarImage10,
+    icon: OscarIcon10,
+    title: "Reduced Inequalities",
+  },
+  {
+    cover: OscarImage11,
+    icon: OscarIcon11,
+    title: "Sustainable Cities And Communities",
+  },
+  {
+    cover: OscarImage12,
+    icon: OscarIcon12,
+    title: "Responsible Consumption And Production",
+  },
+  {
+    cover: OscarImage13,
+    icon: OscarIcon13,
+    title: "Climate Action",
+  },
+  {
+    cover: OscarImage14,
+    icon: OscarIcon14,
+    title: "Life Below Water",
+  },
+  {
+    cover: OscarImage15,
+    icon: OscarIcon15,
+    title: "Life On Land",
+  },
+  {
+    cover: OscarImage16,
+    icon: OscarIcon16,
+    title: "Peace, Justice And Strong Institutions",
+  },
+  {
+    cover: OscarImage17,
+    icon: OscarIcon17,
+    title: "Partnerships For The Goals",
+  },
+];
+
+const LEN = bannerList.length;
+
+export function Banner() {
+  return (
+    <div className="flex flex-col md:flex-row h-[700px] -mt-20">
+      <BannerImpl />
+      <Ceremony />
+    </div>
+  );
+}
+
+function BannerImpl() {
+  const [api, setApi] = useState<CarouselApi>();
+
+  useEffect(() => {
+    if (!api) {
+      return;
+    }
+
+    api.on("settle", () => {
+      // Do something on select.
+      console.log("settle");
+    });
+  }, [api]);
+
+  return (
+    <div
+      style={{
+        width: "calc(100% - 18rem)",
+      }}
+      className={cn("flex-none relative z-10", styles.banner)}
+    >
+      <div className="absolute left-0 top-0 h-full w-full">
+        <Carousel
+          opts={{
+            loop: true,
+            duration: 40,
+            watchDrag: false,
+            watchFocus: false,
+            watchResize: false,
+            watchSlides: false,
+          }}
+          plugins={[
+            Autoplay({
+              delay: 3000,
+            }),
+          ]}
+          setApi={setApi}
+        >
+          <CarouselContent>
+            {bannerList.map((banner) => (
+              <CarouselItem key={banner.title} className="w-full h-full ">
+                <Image
+                  className="w-full h-full object-cover"
+                  src={banner.cover}
+                  width={1920}
+                  height={1080}
+                  alt={banner.title}
+                />
+              </CarouselItem>
+            ))}
+          </CarouselContent>
+        </Carousel>
+      </div>
+      <div className="absolute left-28 top-1/2 -translate-y-1/2 space-y-6">
+        <h1 className={cn(styles["banner-title"], "text-5xl font-bold")}>
+          BGA Oscar
+        </h1>
+        <p className="text-description text-2xl font-bold">
+          Sustainable Innovation Summit: <br />
+          The 17 SDG Challenge
+        </p>
+        <div
+          className={cn(
+            "inline-flex bg-transparent px-6 py-3 font-bold md:w-64 text-center",
+            styles["banner-button"]
+          )}
+        >
+          <span className="mx-auto">Empower the Future</span>
+        </div>
+        <Carousel
+          opts={{
+            loop: true,
+            duration: 60,
+            containScroll: false,
+          }}
+          plugins={[
+            Autoplay({
+              delay: 3000,
+            }),
+            Fade(),
+          ]}
+        >
+          <CarouselContent>
+            {bannerList.map((banner) => (
+              <CarouselItem key={banner.title} className="w-full h-10">
+                <p className="text-sm text-description font-semibold flex items-center gap-2">
+                  <Image
+                    src={banner.icon}
+                    width={40}
+                    height={40}
+                    alt={banner.title}
+                  />
+                  {banner.title.toUpperCase()}
+                </p>
+              </CarouselItem>
+            ))}
+          </CarouselContent>
+        </Carousel>
+      </div>
+    </div>
+  );
+}
+
+function Ceremony() {
+  return (
+    <div
+      className={cn(
+        "flex-none flex flex-col justify-center items-center h-44 w-full md:w-96 md:h-full relative -translate-x-24",
+        styles.ceremony
+      )}
+    >
+      <h3 className="text-2xl font-bold text-center">Ceremony</h3>
+      <p>Stay Tuned</p>
+    </div>
+  );
+}
---
+++ app/oscar/page.tsx
@@ -0,0 +1,16 @@
+import { Banner } from "./Banner";
+import { Evaluation } from "./Evaluation";
+import { OscarFAQ } from "./FAQ";
+import { OscarIntro } from "./OscarIntro";
+import { Partners } from "./Partners";
+import { Prizes } from "./Prize";
+import { SubmissionProcess } from "./SubmissionProcess";
+
+export default function Oscar() {
+  return (
+    <div>
+      <Banner />
+      <OscarIntro />
+    </div>
+  );
+}
