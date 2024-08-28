3a22d8d634e16e791fc02ba1b545c8d3d4cb24f3
right <daocoredev@gmail.com>
2024-08-28 11:32:11 +0800

    update mobile & partner logo

--- app/oscar/SummitIntro.tsx
+++ app/oscar/SummitIntro.tsx
@@ -1,6 +1,10 @@
-export const OscorTitle = ({ text }: { text: string }) => {
-  return <h2 className="font-bold text-oscorActive font-['Inter'] text-[32px]" style={{
-    lineHeight: "41.6px",
+import { useIsMobile } from "@/hooks";
+
+export const OscorTitle = ({ text, center }: { text: string, center?: boolean }) => {
+  const mobile = useIsMobile();
+
+  return <h2 className={`font-bold text-oscorActive font-['Inter'] text-[20px] md:text-[32px] ${center ? "text-center md:text-left" : "text-left"}`} style={{
+    lineHeight: mobile ? "150%" : "41.6px",
     letterSpacing: "-1%"
   }}>{text}</h2>
 }
@@ -8,9 +12,9 @@ export const OscorTitle = ({ text }: { text: string }) => {
 export const SummitInto = () => {
 
   return (
-    <div className="w-full md:w-content m-auto">
-      <OscorTitle text="Sustainable Innovation Summit: The 17 SDG Challenge" />
-      <div className="text-description font-normal font-['Inter'] text-[16px]" style={{ lineHeight: "24px" }}>
+    <div className="w-full md:w-content m-auto px-6 md:px-0">
+      <OscorTitle text="Sustainable Innovation Summit: The 17 SDG Challenge" center/>
+      <div className="text-description font-normal font-['Inter'] text-sm md:text-[16px] text-center md:text-left mt-2" style={{ lineHeight: "24px" }}>
         In a world guided by the United Nations' 17 Sustainable Development Goals, we stand on the brink of a new era. Blockchain technology, with its transformative power, offers us the chance to tackle global challenges like never before. This hackathon is not just a competition—it's a journey where innovators from around the world unite to create solutions that could reshape our future. Join us as we explore the untapped potential of blockchain to drive sustainable change.
       </div>
     </div>
--- app/page.tsx
+++ app/page.tsx
@@ -7,6 +7,7 @@ import { initSensors, trackSensors } from "@/lib/sensors";
 import { What } from "./home/what";
 import { How } from "./home/how";
 import { Videos } from "./home/Video";
+import { OscarIntro } from "./oscar/OscarIntro";
 
 export default function Home() {
   useEffect(() => {
