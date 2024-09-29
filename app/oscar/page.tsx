46f662740b0aeb7e187cf2a7e06bf3fd7d408b9c
melon <melon@MelonMacBook-Pro.local>
2024-09-29 12:30:10 +0800

    vote form field sort && shared card

--- app/oscar/page.tsx
+++ app/oscar/page.tsx
@@ -1,5 +1,7 @@
+import { Metadata } from "next";
 import { Banner } from "./Banner";
 import { OscarIntro } from "./OscarIntro";
+import OscarImage1 from "@/assets/oscar/oscar1.png";
 
 export default function Oscar() {
   return (
@@ -8,4 +10,38 @@ export default function Oscar() {
       <OscarIntro />
     </div>
   );
+}
+
+export async function generateMetadata(): Promise<Metadata> {
+  const coverImage = OscarImage1.src;
+
+  const name = "Oscar";
+  const desc = "BGA Oscar";
+  return {
+    twitter: {
+      card: "summary_large_image",
+      title: name,
+      creator: "@chainforgood",
+      site: "@chainforgood",
+      description: desc,
+      images: [
+        {
+          url: coverImage,
+          alt: name,
+        },
+      ],
+    },
+    openGraph: {
+      title: name,
+      description: desc,
+      url: "https://www.blockchainforgood.xyz/oscar",
+      images: [
+        {
+          url: coverImage,
+          alt: name,
+        },
+      ],
+      type: "website",
+    },
+  };
 }
--- app/vote/[id]/skeleton-loading.tsx
+++ app/vote/[id]/skeleton-loading.tsx
@@ -2,7 +2,7 @@ import { Skeleton } from "@/components/ui/skeleton";
 
 export function Loading() {
   return (
-    <div className="w-full md:w-content mx-auto">
+    <div className="w-full max-w-[768px] mx-auto">
       <Skeleton className="w-full h-48 md:h-72"></Skeleton>
       <Skeleton className="w-full h-48 md:h-72 my-4"></Skeleton>
       <Skeleton className="w-full h-48 md:h-72"></Skeleton>
