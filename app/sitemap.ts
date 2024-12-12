4240092cc7004395a40f192cf4abe50548547c9a
melon <melon@MelonMacBook-Pro.local>
2024-12-12 14:11:50 +0800

    upload docker image

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,23 +1,18 @@
-import { MetadataRoute } from 'next'
-import { API_PATH } from './news/api'
+import { MetadataRoute } from "next";
+import { API_PATH } from "./news/api";
 import { API_URL, VISITED_PRODUCTION_URL } from "@/constants";
-import { PUBLISHED } from './news/enums';
-import { INews, IPageData } from './news/types';
-
-
+import { PUBLISHED } from "./news/enums";
+import { INews, IPageData } from "./news/types";
 
 const getAllNews = async () => {
-  const params = {
-    publish: PUBLISHED.PUBLISHED,
-    current: 1,
-    pageSize: 1000,
-  }
-  const res = await fetch(`${API_URL}${API_PATH.GET_NEWS}?`)
-  return res.json()
-}
- 
+  const res = await fetch(
+    `${API_URL}${API_PATH.GET_NEWS}?publish=${PUBLISHED.PUBLISHED}&current=1&pageSize=200`
+  );
+  return res.json();
+};
+
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
-  const newsList = ((await getAllNews()).data as IPageData<INews>).list
+  const newsList = ((await getAllNews()).data as IPageData<INews>).list ?? [];
   return [
     {
       url: VISITED_PRODUCTION_URL,
@@ -25,7 +20,7 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
       priority: 1,
     },
     {
-      url: VISITED_PRODUCTION_URL + 'news',
+      url: VISITED_PRODUCTION_URL + "news",
       lastModified: new Date(),
       priority: 0.5,
     },
@@ -35,9 +30,9 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
       priority: 0.8,
     })),
     {
-      url: VISITED_PRODUCTION_URL + 'incubation',
+      url: VISITED_PRODUCTION_URL + "incubation",
       lastModified: new Date(),
       priority: 0.8,
     },
-  ]
+  ];
 }
